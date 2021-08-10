019e2d1d6b4978003199d5b9cdbe6e3e1de4ff8a
jonathancode <parkj97@gmail.com>
2021-08-10 10:55:42 -0700

    expenses additional info

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -6,16 +6,25 @@ const Expenses = () => {
       duedate: "3-3-21",
     },
     {
-        name:"Netflix",
-        cost: 15,
-        duedate: "3-30-21"
-    }
+      name: "Netflix",
+      cost: 15,
+      duedate: "3-30-21",
+    },
   ];
 
   return (
     <div>
       <h4>Expenses</h4>
-      <div className="expenseForm"></div>
+      <div className="expenseForm">
+        <form>
+          <label id="expenseName">Item Name</label>
+          <input htmlFor="expenseName" type="text" />
+          <label id="expenseCost">Item Cost</label>
+          <input htmlFor="expenseCost" type="Number" />
+          <label id="expenseDate">Date Due</label>
+          <input htmlFor="expenseDate" type="date" />
+        </form>
+      </div>
       <div className="expenseItem">
         {expenseItems.map((expense) => (
           <div>
