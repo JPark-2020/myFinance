1eb5a1ff00fea0fc8bc03f9c0e2937efceceec89
jonathancode <parkj97@gmail.com>
2021-08-09 17:14:23 -0700

    first commit

---
+++ src/util/firebase.js
@@ -0,0 +1,17 @@
+import firebase from 'firebase';
+import config from './config';
+var firebaseConfig = {
+    apiKey: config.apiKey,
+    authDomain: config.authDomain,
+    projectId: config.projectId,
+    storageBucket: config.storageBucket,
+    messagingSenderId: config.messagingSenderId,
+    appId: config.appId
+  };
+  
+  // Initialize Firebase
+  const fire = firebase.initializeApp(firebaseConfig);
+  
+  export default fire; 
+
+// const db = firebase.firestore();
