d8b050f186ab004c3778be3fca6007f4d782d706
jonathancode <parkj97@gmail.com>
2021-08-13 20:51:21 -0700

    deposit funds works

--- src/components/Banking/Banking.js
+++ src/components/Banking/Banking.js
@@ -1,79 +1,102 @@
-import React, { useState, useEffect } from 'react';
-import {fire, db} from '../../util/firebase';
-
+import React, { useState, useEffect } from "react";
+import { fire, db } from "../../util/firebase";
+import { v4 as uuidv4 } from "uuid";
 
 const Banking = () => {
+  const [balance, setBalance] = useState();
+  const [bankingItems, setBankingItems] = useState();
+  const [withdraw, setWithdraw] = useState();
+  const [deposit, setDeposit] = useState();
+  const [loading, setLoading] = useState(false);
 
-    const [balance, setBalance] = useState();
-    const [bankingItems, setBankingItems] = useState();
-    const [withdraw, setWithdraw] = useState();
-    const [deposit, setDeposit] = useState()
-    const [loading, setLoading] = useState(false);
+  const ref = db.collection("userbalance");
+  
 
-    const ref = db.collection("userbalance");
-
-
-    function getUserBalance() {
-        setLoading(true);
-        const userBalance = ref.get().where('user', '==', fire.auth().currentUser.uid)
-        setBalance(userBalance);
+  function getUserBalance() {
+      const test = 0; 
+    setLoading(true);
+    const userBalance = ref
+      .where("id", "==", fire.auth().currentUser.uid)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          const data = doc.data();
+          const balanceNumber = data.balance;
+          setBalance(balanceNumber);
+          console.log(balanceNumber)
+        });
         setLoading(false);
-    }
+      });
+  }
 
-    function getBankingHistory() {
-        setLoading(true);
-        ref.orderBy("date", "desc").onSnapshot((querySnapshot) => {
-            const items = [];
-            querySnapshot.forEach((doc) =>{
-                items.push(doc.data())
-            });
-            setBankingItems(items);
-            setLoading(false);
-        })
-    }
+  function getBankingHistory() {
+    setLoading(true);
+    ref.orderBy("date", "desc").onSnapshot((querySnapshot) => {
+      const items = [];
+      querySnapshot.forEach((doc) => {
+        items.push(doc.data());
+      });
+      setBankingItems(items);
+      setLoading(false);
+    });
+  }
 
-    useEffect(() => {
-        getUserBalance();
-        getBankingHistory();
-    }, []);
+  useEffect(() => {
+    getUserBalance();
+  }, []);
 
-    function addBalance(targetBalance){
-          
-    }
+  function addBalance(updateBalance) {
+   ref.doc(updateBalance.id).set(updateBalance).catch((err) => {
+       console.log(err)
+   })
+  }
 
+  const addBalanceHandler = (event) => {
+    event.preventDefault();
 
+    
+    const additionalBalance = balance ? (parseInt(balance) + parseInt(deposit)) : (parseInt(deposit));
+    addBalance({
+      balance: additionalBalance,
+      id: fire.auth().currentUser.uid
+    })
+    setBalance(additionalBalance);
+  };
 
+  return (
+    <React.Fragment>
+      <div>
+        <h3>Banking</h3>
+        <h4>Current Balance: {balance}</h4>
+      </div>
 
-    return (
-        <React.Fragment>
-            <div>
-                <h3>Banking</h3>
-                <h3>Current Balance: {balance}</h3>
-            </div>
+      <div>
+        <h3>Deposit Funds</h3>
+        <form onSubmit={addBalanceHandler}>
+          <input
+            type="number"
+            value={deposit}
+            onChange={(e) => setDeposit(e.target.value)}
+          />
+          <label>Amount</label>
+          <button type="submit">Deposit Funds</button>
+        </form>
+      </div>
+      <div>
+        <h3>Withdraw Funds</h3>
+        <form>
+          <input type="number" />
+          <label>Amount</label>
+          <button type="submit">Withdraw Funds</button>
+        </form>
+      </div>
+      <hr />
+      <br />
+      <div>
+        <h4>History</h4>
+      </div>
+    </React.Fragment>
+  );
+};
 
-            <div>
-                <h3>Deposit Funds</h3>
-                <form>
-                    <input type="number" />
-                    <label>Amount</label>
-                    <button type="submit">Deposit Funds</button>
-                </form>
-            </div>
-            <div>
-                <h3>Withdraw Funds</h3>
-                <form>
-                    <input type="number"/>
-                    <label>Amount</label>
-                    <button type="submit" >Withdraw Funds</button>
-                </form>
-            </div>
-            <hr />
-            <br/>
-            <div>
-                <h4>History</h4>
-            </div>
-        </React.Fragment>
-    )
-}
-
-export default Banking; 
+export default Banking;
